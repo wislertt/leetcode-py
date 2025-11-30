4616f16c7656d3699e4c686b21df0258b2ca36d9
Shravan Chandra <52000043+shravnchandr@users.noreply.github.com>
2025-11-30 22:54:30 +0530

    fix(tests): add more test cases to meet minimum requirement of 10

--- leetcode/palindrome_partitioning/test_solution.py
+++ leetcode/palindrome_partitioning/test_solution.py
@@ -11,7 +11,23 @@ class TestPalindromePartitioning:
         self.solution = Solution()
 
     @logged_test
-    @pytest.mark.parametrize("s, expected", [("aab", [["a", "a", "b"], ["aa", "b"]]), ("a", [["a"]])])
+    @pytest.mark.parametrize(
+        "s, expected",
+        [
+            ("aab", [["a", "a", "b"], ["aa", "b"]]),
+            ("a", [["a"]]),
+            ("ab", [["a", "b"]]),
+            ("aa", [["a", "a"], ["aa"]]),
+            ("abc", [["a", "b", "c"]]),
+            ("aba", [["a", "b", "a"], ["aba"]]),
+            ("aaa", [["a", "a", "a"], ["a", "aa"], ["aa", "a"], ["aaa"]]),
+            ("abba", [["a", "b", "b", "a"], ["a", "bb", "a"], ["abba"]]),
+            ("zz", [["z", "z"], ["zz"]]),
+            ("efe", [["e", "f", "e"], ["efe"]]),
+            ("xyx", [["x", "y", "x"], ["xyx"]]),
+            ("noon", [["n", "o", "o", 'n'], ['n', 'oo', 'n'], ['noon']]),
+        ],
+    )
     def test_partition(self, s: str, expected: list[list[str]]):
         result = run_partition(Solution, s)
         assert_partition(result, expected)
